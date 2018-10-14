fe83fbefcb74efe5fa00fe8991dd06c1617e5104
libetl <libe4@free.fr>
2018-10-14 19:03:31 +0200

    adding babel for compatibility with older browsers

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ module.exports = [{
     output: {path: path.resolve(__dirname, 'dist'), filename: 'main.js'},
     module: {
         rules: [{
-                 test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/, use: 'file-loader?name=[name].[ext]'
+            test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/, use: 'file-loader?name=[name].[ext]'
+        },
+            {
+                test: /\.js$/,
+                use: {loader: 'babel-loader',
+                    options: {presets: ['@babel/preset-env']}}
             },
             {
                 test: /\.s?css$/,
