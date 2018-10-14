dd5dcd6ff0d8d38016edac8719a49e024f37c823
libetl <libe4@free.fr>
2018-10-14 16:10:25 +0200

    isolated render part

---
+++ src/render.js
@@ -0,0 +1,23 @@
+window.reRender = taskList => {
+    document.querySelector('#table').innerHTML = renderTable(taskList)
+    document.querySelector('#table').dataset.upgraded = ''
+    componentHandler && componentHandler.upgradeElement(document.querySelector('#table'))
+    document.querySelector('#text').value = ''
+}
+
+window.renderTable = taskList => `<table id="table" class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp" style="float:left">
+                <thead>
+                <tr>
+                    <th class="mdl-data-table__cell--non-numeric">Task name</th>
+                    <th>Created on</th>
+                </tr>
+                </thead>
+                <tbody id="list">
+                    ${taskList.map(element => `<tr>
+                        <td class="mdl-data-table__cell--non-numeric">${!element.done ? element.message :
+    `<span style="text-decoration: line-through;">${element.message}</span>`}</td>
+                        <td>${!element.done ? element.date :
+    `<span style="text-decoration: line-through;">${element.date}</span>`}</td>`)
+    .join('')}
+                </tbody>
+            </table>`
